60752fc4f2c17c97c7c1ff7b4068b2efd335b772
Rayane <raay.rs4@gmail.com>
2024-12-06 15:52:41 -0300

    add login como primeira pagina

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,14 +1,13 @@
 import { Routes } from '@angular/router';
-import { LoginComponent } from './login/login.component';
-import { CadastroComponent } from './cadastro/cadastro.component';
+import { LoginComponent } from './pages/login/login.component'; 
+import { CadastroComponent } from './pages/cadastro/cadastro.component'; 
 import { UsuariosComponent } from './usuarios/usuarios.component';
-import { HomeComponent } from './home/home.component';
+import { HomeComponent } from './pages/home/home.component'; 
 
 export const routes: Routes = [
     {
         path: '',
-        redirectTo: 'home',
-        pathMatch: 'full'
+        component: LoginComponent
     },
     {
         path: 'home',
