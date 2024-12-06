7f757106066ddcd04b1de701ae7c308b04c279fd
Rayane <raay.rs4@gmail.com>
2024-12-06 16:21:47 -0300

    update rotas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,12 +2,13 @@ import { Routes } from '@angular/router';
 import { LoginComponent } from './pages/login/login.component'; 
 import { CadastroComponent } from './pages/cadastro/cadastro.component'; 
 import { UsuariosComponent } from './usuarios/usuarios.component';
-import { HomeComponent } from './pages/home/home.component'; 
+import { HomeComponent } from './pages/home/home.component';
 
 export const routes: Routes = [
     {
         path: '',
-        component: LoginComponent
+        redirectTo: 'home',
+        pathMatch: 'full'
     },
     {
         path: 'home',
